9d3c1743140dd310ed043705878bcd72b7cf8e68
nathan.chevalier <nathan.chevalier@epita.fr>
2022-05-07 16:08:21 +0200

    Pong: Update: Mono to class Pong Game

--- pong/pong.py
+++ pong/pong.py
@@ -1,33 +1,46 @@
 # Pong System
 from microbit import *
-import random
 
-ball = (3, 2)
-direction = (1, 1)
-plateX = 1
-vit = 1
+class Pong:
 
-random.randint(0, 5)
+    def __init__(self):
+        self.ball = (3, 2)
+        self.direction = (1, 1)
+        self.plateX = 1
+        self.vit = 1
+
+    def display_pong(self):
+        display.clear()
+        display.set_pixel(self.ball[0], self.ball[1], 9)
+        display.set_pixel(self.plateX, 4, 9)
+        display.set_pixel(self.plateX + 1, 4, 9)
+
+    def update_plate(self):
+        if button_b.was_pressed() and self.plateX < 3:
+            self.plateX += 1
+        elif button_a.was_pressed() and self.plateX > 0:
+            self.plateX -= 1
+
+    def update_ball(self):
+        if self.ball[0] == 0:
+            self.direction = (1, self.direction[1])
+        elif self.ball[0] == 4:
+            self.direction = (-1, self.direction[1])
+
+        if self.ball[1] == 0:
+            self.direction = (self.direction[0], 1)
+        elif self.ball[1] == 3:
+            self.direction = (self.direction[0], -1)
+
+        self.ball = (
+            self.ball[0] + self.direction[0],
+            self.ball[1] + self.direction[1]
+        )
+
+game = Pong()
 
 while True:
-    if ball[0] == 0:
-        direction = (1, direction[1])
-    elif ball[0] == 4:
-        direction = (-1, direction[1])
-
-    if ball[1] == 0:
-        direction = (direction[0], 1)
-    elif ball[1] == 3:
-        direction = (direction[0], -1)
-
-    if button_b.was_pressed() and plateX < 3:
-        plateX += 1
-    elif button_a.was_pressed() and plateX > 0:
-        plateX -= 1
-
-    ball = (ball[0] + direction[0], ball[1] + direction[1])
-    display.clear()
-    display.set_pixel(ball[0], ball[1], 9)
-    display.set_pixel(plateX, 4, 9)
-    display.set_pixel(plateX + 1, 4, 9)
-    sleep(300 / vit)
+    game.update_ball()
+    game.update_plate()
+    game.display_pong()
+    sleep(300 / game.vit)
